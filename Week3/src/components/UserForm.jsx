eca1afd9d10b73c20832cbc33ff85f99219bd732
NgocHai190605 <hoangngochai190605@gmail.com>
2026-01-31 10:26:10 +0700

    ReactTuan3

---
+++ Week3/src/components/UserForm.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+
+function UserForm() {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    age: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Name: ${user.name}\nEmail: ${user.email}\nAge: ${user.age}`);
+  };
+
+  return (
+    <div>
+      <h2>User Information Form</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name: </label>
+          <input
+            type="text"
+            name="name"
+            value={user.name}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <label>Email: </label>
+          <input
+            type="email"
+            name="email"
+            value={user.email}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <label>Age: </label>
+          <input
+            type="number"
+            name="age"
+            value={user.age}
+            onChange={handleChange}
+          />
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default UserForm;
